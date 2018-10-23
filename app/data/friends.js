a5ed2bc4d95b69b196b6bc23a87ecc8c7827c30c
Tanya Valdez <TValdez@digmap.com>
2018-10-23 00:50:43 -0700

    Add friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,23 @@
+//Array to store friend data
+const friendArray = [
+  {
+    name: "Tom",
+    photo: "https://ibb.co/d8z4kq",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+  },
+  {
+    name: "Kim",
+    photo: "https://ibb.co/fmzgWV",
+    scores: ["5", "3", "1", "2", "1", "5", "3", "5", "2", "5"]
+  },
+  {
+    name: "Kanye",
+    photo: "https://ibb.co/kpKgWV",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+  }
+];
+
+module.exports = friendArray
+  
+
+
