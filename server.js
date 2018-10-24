83dbf75b82bcb35e660b3877fbac08d6604c0647
Tanya Valdez <TValdez@digmap.com>
2018-10-23 18:56:33 -0700

    update app.listen

--- server.js
+++ server.js
@@ -15,4 +15,4 @@ app.use(bodyparser.text());
 require(path.join(__dirname, "./app/routes/apiRoutes"))(app);
 require(path.join(__dirname, "./app/routes/htmlRoutes"))(app);
 
-app.listen(3030, () => console.log('http://localhost:3030'))
+app.listen(process.env.PORT || 3030, () => console.log('http://localhost:3030'))
